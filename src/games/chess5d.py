d0acce214b28e2d3417d5c547d88583d71b65bb3
pranav <pranavraj575@gmail.com>
2026-02-28 23:00:56 -0500

    added new chess5d

---
+++ src/games/chess5d.py
@@ -0,0 +1,125 @@
+import copy
+import itertools
+
+import numpy as np
+import torch
+from .game import Game
+
+
+class Chess5d(Game):
+
+    # reserve a separate index for a blocked board (i.e. board that does not exist yet)
+    # this is necessary since knights can jump over blocked boards, but other pieces cannot
+    BLOCKED= 18
+
+    EMPTY = 0
+
+    # a pawn cannot be both unmoved and passantable, so we do not need to add that
+    PAWN = 1
+
+    UNMOVED_PAWN = 14
+    PASSANTABLE_PAWN = 17
+
+    KING = 2
+    UNMOVED_KING = 15
+
+    ROOK = 3
+    UNMOVED_ROOK = 16
+
+    KNIGHT = 4
+    BISHOP = 5
+    QUEEN = 6  # attacks on any number of diagonals
+
+    PRINCESS = 10  # combo of rook and bishop
+    UNICORN = 11  # attacks on triagonls
+    DRAGON = 12  # attacks on quadragonals
+
+    def num_agents(self):
+        return 2
+
+    def init_state(self):
+        """
+        Initial state of the environment.
+        Returns:
+            The initial state of the environment.
+        """
+        back_rank=torch.tensor([self.UNMOVED_ROOK,
+                   self.KNIGHT,
+                   self.BISHOP,
+                   self.QUEEN,
+                   self.UNMOVED_KING,
+                   self.BISHOP,
+                   self.KNIGHT,
+                   self.UNMOVED_ROOK,
+         ])
+        board=torch.zeros(8,8)
+        board[0]=back_rank
+        board[1]=self.UNMOVED_PAWN
+        board[-2]=-self.UNMOVED_PAWN
+        board[-1]=-back_rank
+        return board, 0
+
+    def player(self, state):
+        raise NotImplementedError
+
+    def step(self, state, action):
+        """
+        Update the environment.
+
+        Args:
+            state: env state.
+            action: The actions taken by the agents.
+        Returns:
+        A tuple containing:
+            (new state,
+            auxiliary information dictionary)
+        """
+        raise NotImplementedError
+
+    def is_terminal(self, state):
+        """
+        Update the environment.
+
+        Args:
+            state: env state.
+        Returns:
+        whether state is terminal
+        """
+        raise NotImplementedError
+
+    def agent_observe(self, state):
+        """
+        agent observations
+        Args:
+            state: The state of the environment.
+        """
+        raise NotImplementedError
+
+    def action_mask(self, state):
+        """
+        possible actions to take
+        Args:
+            state: The state of the environment.
+        """
+        raise NotImplementedError
+
+    def critic_observe(self, state):
+        """
+        critic observations
+        Args:
+            state: The state of the environment.
+        """
+        raise NotImplementedError
+
+    def example_observation(self):
+        # This assumes the environment provides same observation shape to both agents
+        state = self.init_state()
+        return self.agent_observe(state=state)
+
+    def example_action_mask(self):
+        state = self.init_state()
+        return self.action_mask(state=state)
+if __name__=='__main__':
+    c=Chess5d()
+    b=c.init_state()
+    print(b)
--- src/games/tictactoe/__init__.py
+++
@@ -1,8 +0,0 @@
-from aleph0.examples.tictactoe.game.tictactoe import Toe
-from aleph0.examples.tictactoe.game.ultimate import LessUltimateToe, UltimateToe
-
-__all__ = [
-    'Toe',
-    'UltimateToe',
-    'LessUltimateToe',
-]
