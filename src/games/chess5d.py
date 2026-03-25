c78e4df42d84418bbadc67268c8a7f505e6b736c
pranav <prajbhan@cs.cmu.edu>
2026-03-25 10:28:28 -0400

    minor update

--- src/games/chess5d.py
+++ src/games/chess5d.py
@@ -10,12 +10,14 @@ class State:
     board: torch.Tensor
     player: int
     piece_held: int
+    center_timeline: int
 
 
 class Chess5d(Game):
     # reserve a separate index for a blocked board (i.e. board that does not exist yet)
     # this is necessary since knights can jump over blocked boards, but other pieces cannot
-    BLOCKED = 18
+    BLOCKED = 19
+    REMOVED = 18
 
     EMPTY = 0
 
@@ -64,32 +66,38 @@ class Chess5d(Game):
             board=board,
             player=0,
             piece_held=-1,
+            center_timeline=0,
         )
 
     def player(self, state):
         return state.player
 
     def step(self, state, action):
+        special_action, board_action = action
         new_board = state.board.clone()
         if state.piece_held < 0:
+            # assert special_action<0 # cannot end turn while piece is held
             # pick a square
-            piece = state.board[*action]
-            new_board[*action] = self.EMPTY
+            piece = state.board[*board_action]
+            new_board[*board_action] = self.REMOVED
             print(piece)
-            pass
         else:
-            pass
+            new_board = torch.where(
+                torch.eq(new_board, self.REMOVED), self.EMPTY, new_board
+            )
+            # TODO: check en passant, castling, captures, etc.
+            print(new_board)
         raise NotImplementedError
 
     def agent_observe(self, state):
         # TODO: potentially flip board for black player
-        return state.board
+        return state.board, torch.tensor([state.piece_held])
 
     def action_mask(self, state):
         raise NotImplementedError
 
     def critic_observe(self, state):
-        raise NotImplementedError
+        return state.board, torch.tensor([state.piece_held])
 
 
 if __name__ == "__main__":
